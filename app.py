de5b3d0126653d438e9cd1077718184c4109d544
Richard Bramley <rabramley@gmail.com>
2021-09-16 11:05:19 +0100

    Output all information

--- app.py
+++ app.py
@@ -1,106 +1,71 @@
-"""An example program that uses the elsapy module"""
-
 import os
+import csv
 from elsapy.elsclient import ElsClient
-from elsapy.elsprofile import ElsAuthor, ElsAffil
-from elsapy.elsdoc import FullDoc, AbsDoc
-from elsapy.elssearch import ElsSearch
+from elsapy.elsprofile import ElsAuthor
 from dotenv import load_dotenv
 
 
+class BrcElsAuthor(ElsAuthor):
+    def __init__(self, scopus_id):
+        self.scopus_id = scopus_id
+        super().__init__(uri=f'https://api.elsevier.com/content/author/author_id/{self.scopus_id}')
+
+    @property
+    def citation_count(self):
+        return self.data[u'coredata'][u'citation-count']
+
+    @property
+    def cited_by_count(self):
+        return self.data[u'coredata'][u'cited-by-count']
+
+    @property
+    def document_count(self):
+        return self.data[u'coredata'][u'document-count']
+
+    @property
+    def h_index(self):
+        return self.data[u'h-index']
+
+COL_SCOPUS_ID = 'scopus_id'
+COL_FULL_NAME = 'full_name'
+COL_DOCUMENT_COUNT = 'document_count'
+COL_CITATION_COUNT = 'citation_count'
+COL_H_INDEX = 'h_index'
+
 load_dotenv()
 
+ids = []
 
-## Initialize client
+with open('author_ids.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    ids = [a[COL_SCOPUS_ID] for a in reader]
+
 client = ElsClient(os.environ["API_KEY"])
-# client.inst_token = os.environ["INSTTOKEN"]
 
-## Author example
-# Initialize author with uri
-my_auth = ElsAuthor(uri='https://api.elsevier.com/content/author/author_id/7005783434')
+with open('output.csv', 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=[
+        COL_SCOPUS_ID,
+        COL_FULL_NAME,
+        COL_DOCUMENT_COUNT,
+        COL_CITATION_COUNT,
+        COL_H_INDEX,
+    ])
 
-# Read author data, then write to disk
-if my_auth.read(client):
-    print ("my_auth.full_name: ", my_auth.full_name)
-    my_auth.write()
-else:
-    print ("Read author failed.")
+    writer.writeheader()
 
-# ## Affiliation example
-# # Initialize affiliation with ID as string
-# my_aff = ElsAffil(affil_id = '60101411')
-# if my_aff.read(client):
-#     print ("my_aff.name: ", my_aff.name)
-#     my_aff.write()
-# else:
-#     print ("Read affiliation failed.")
+    for id in filter(lambda id: id, ids):
+        author = BrcElsAuthor(scopus_id=id)
 
-# ## Scopus (Abtract) document example
-# # Initialize document with ID as integer
-# scp_doc = AbsDoc(scp_id = 84872135457)
-# if scp_doc.read(client):
-#     print ("scp_doc.title: ", scp_doc.title)
-#     scp_doc.write()   
-# else:
-#     print ("Read document failed.")
+        if not author.read(client):
+            raise RuntimeError('Reading author details failed')
 
-# ## ScienceDirect (full-text) document example using PII
-# pii_doc = FullDoc(sd_pii = 'S1674927814000082')
-# if pii_doc.read(client):
-#     print ("pii_doc.title: ", pii_doc.title)
-#     pii_doc.write()   
-# else:
-#     print ("Read document failed.")
+        if not author.read_metrics(client):
+            raise RuntimeError('Reading metrics details failed')
 
-# ## ScienceDirect (full-text) document example using DOI
-# doi_doc = FullDoc(doi = '10.1016/S1525-1578(10)60571-5')
-# if doi_doc.read(client):
-#     print ("doi_doc.title: ", doi_doc.title)
-#     doi_doc.write()   
-# else:
-#     print ("Read document failed.")
-
-
-# ## Load list of documents from the API into affilation and author objects.
-# # Since a document list is retrieved for 25 entries at a time, this is
-# #  a potentially lenghty operation - hence the prompt.
-# print ("Load documents (Y/N)?")
-# s = input('--> ')
-
-# if (s == "y" or s == "Y"):
-
-#     ## Read all documents for example author, then write to disk
-#     if my_auth.read_docs(client):
-#         print ("my_auth.doc_list has " + str(len(my_auth.doc_list)) + " items.")
-#         my_auth.write_docs()
-#     else:
-#         print ("Read docs for author failed.")
-
-#     ## Read all documents for example affiliation, then write to disk
-#     if my_aff.read_docs(client):
-#         print ("my_aff.doc_list has " + str(len(my_aff.doc_list)) + " items.")
-#         my_aff.write_docs()
-#     else:
-#         print ("Read docs for affiliation failed.")
-
-# ## Initialize author search object and execute search
-# auth_srch = ElsSearch('authlast(keuskamp)','author')
-# auth_srch.execute(client)
-# print ("auth_srch has", len(auth_srch.results), "results.")
-
-# ## Initialize affiliation search object and execute search
-# aff_srch = ElsSearch('affil(amsterdam)','affiliation')
-# aff_srch.execute(client)
-# print ("aff_srch has", len(aff_srch.results), "results.")
-
-# ## Initialize doc search object using Scopus and execute search, retrieving 
-# #   all results
-# doc_srch = ElsSearch("AFFIL(dartmouth) AND AUTHOR-NAME(lewis) AND PUBYEAR > 2011",'scopus')
-# doc_srch.execute(client, get_all = True)
-# print ("doc_srch has", len(doc_srch.results), "results.")
-
-# ## Initialize doc search object using ScienceDirect and execute search, 
-# #   retrieving all results
-# doc_srch = ElsSearch("star trek vs star wars",'sciencedirect')
-# doc_srch.execute(client, get_all = False)
-# print ("doc_srch has", len(doc_srch.results), "results.")
+        writer.writerow({
+            COL_SCOPUS_ID: id,
+            COL_FULL_NAME: author.full_name,
+            COL_DOCUMENT_COUNT: author.document_count,
+            COL_CITATION_COUNT: author.citation_count,
+            COL_H_INDEX: author.h_index,
+        })
